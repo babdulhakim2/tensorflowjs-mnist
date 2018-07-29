383da7312db068f6b51dfd6f4eefff3a87ea33d9
Abdul <Abdulhakim>
2018-07-29 15:45:37 +0100

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
 import { Component, ViewChild, OnInit } from '@angular/core';
 import { DrawableDirective } from './drawable.directive';
 
-
+import * as tf from '@tensorflow/tfjs';
+import { image } from '@tensorflow/tfjs';
 
 @Component({
   selector: 'app-root',
@@ -10,27 +11,32 @@ import { DrawableDirective } from './drawable.directive';
 })
 export class AppComponent implements OnInit {
 
-  // linearModel: tf.Sequential;
-  // prediction: any;
-
+   model:tf.Model;
+   predictions:any;
 
 
   ngOnInit() {
-    this.loadModel();
+   this.loadModel()
   }
 
-
+  async loadModel(){
+    this.model = await tf.loadModel('/assets/model.json');
+  }
   //// LOAD PRETRAINED KERAS MODEL ////
 
-  async loadModel() {
-   
+  async predict(imageData:ImageData){
+    const pred = await tf.tidy(()=>{
+      let img = tf.fromPixels(imageData, 1) 
+      // console.log(img);
+      img = img.reshape([1,28,28,1]);
+      img = tf.cast(img,'float32');
+
+      const output = this.model.predict(img) as any;
+
+      this.predictions = Array.from(output.dataSync());
+      console.log(this.predictions);
+    })
   }
-
-  async predict(imageData: ImageData) {
-
-
-
-  }
-
+  
 }
 
